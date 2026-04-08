4ec6739249e25e40194cde77f684d927d4538b38
Muhammad Sahrul Ikhsan <sahrulikhsan44@gmail.com>
2026-04-08 13:55:36 +0700

    mengumpulkan lab minggu 4 mengubah view dari FBV menjadi CBV

--- 23758018_iet_2026/main_app/views.py
+++ 23758018_iet_2026/main_app/views.py
@@ -1,6 +1,55 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views import View
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, redirect
+from .models import Report
 
-# Create your views here.
+# LIST (READ)
+class ReportListView(ListView):
+    model = Report
+    template_name = 'main_app/home.html'
+    context_object_name = 'reports'
 
-def home(request):
-    return render(request, 'main_app/home.html')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['status_choices'] = Report._meta.get_field('status').choices
+        return context
+
+
+# DETAIL
+class ReportDetailView(DetailView):
+    model = Report
+    template_name = 'main_app/detail_report.html'
+
+
+# CREATE
+class ReportCreateView(CreateView):
+    model = Report
+    fields = ['title', 'category', 'description', 'location']
+    template_name = 'main_app/add_report.html'
+    success_url = reverse_lazy('report_list')
+
+
+# UPDATE
+class ReportUpdateView(UpdateView):
+    model = Report
+    fields = ['title', 'category', 'description', 'location']
+    template_name = 'main_app/edit_report.html'
+    success_url = reverse_lazy('report_list')
+
+
+# DELETE
+class ReportDeleteView(DeleteView):
+    model = Report
+    template_name = 'main_app/delete_report.html'
+    success_url = reverse_lazy('report_list')
+
+
+# UPDATE STATUS (WORKFLOW)
+class ReportUpdateStatusView(View):
+    def post(self, request, pk):
+        report = get_object_or_404(Report, pk=pk)
+        new_status = request.POST.get('status')
+        report.status = new_status
+        report.save()
+        return redirect('report_list')

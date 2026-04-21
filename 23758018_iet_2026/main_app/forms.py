9f9a54edf39e35773275955b54754f56129dca50
Muhammad Sahrul Ikhsan <sahrulikhsan44@gmail.com>
2026-04-21 14:57:52 +0700

    Memperbaiki tampilan dari add form dan edit form

--- 23758018_iet_2026/main_app/forms.py
+++ 23758018_iet_2026/main_app/forms.py
@@ -7,8 +7,30 @@ class ReportForm(forms.ModelForm):
         fields = ['title', 'category', 'description', 'location']
 
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'category': forms.TextInput(attrs={'class': 'form-control'}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
-            'location': forms.TextInput(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Masukkan judul laporan',
+                'required': True
+            }),
+            'category': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Contoh: Jalan Rusak, Saluran Air, dll',
+                'required': True
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 5,
+                'placeholder': 'Jelaskan detail masalah yang ingin dilaporkan',
+                'required': True
+            }),
+            'location': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Masukkan lokasi/alamat lengkap',
+                'required': True
+            }),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.error_messages['required'] = 'Kolom ini harus diisi'

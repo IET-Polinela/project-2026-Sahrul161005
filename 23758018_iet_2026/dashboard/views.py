93bbdec6ec9f26390d08bddf7a4cf1bb514d7681
Muhammad Sahrul Ikhsan <sahrulikhsan44@gmail.com>
2026-04-29 15:07:11 +0700

    pengumpulan memperbaiki tampilan dashboard

--- 23758018_iet_2026/dashboard/views.py
+++ 23758018_iet_2026/dashboard/views.py
@@ -2,12 +2,24 @@ from django.views.generic import TemplateView, View
 from django.http import JsonResponse
 from main_app.models import Report
 from django.db.models import Count
-
+from django.views.generic import TemplateView
+from main_app.models import Report
 
 # PAGE DASHBOARD
 class DashboardView(TemplateView):
-    template_name = 'dashboard/dashboard.html'
+    template_name = 'dashboard/dashboard.html'  # sesuaikan folder kamu
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        reports = Report.objects.all()
+
+        context['reports'] = reports
+        context['in_progress'] = Report.objects.filter(status='IN_PROGRESS').count()
+        context['resolved'] = Report.objects.filter(status='RESOLVED').count()
+        context['active'] = Report.objects.exclude(status='RESOLVED').count()
+
+        return context
 
 # API STATUS
 class StatusChartData(View):
